0848af3963046bdf303252f1f1661c2f62f8fda6
aclayton <aaronc@protonmail.ch>
2018-01-19 15:11:32 -0500

    oauth

--- server/auth/trakt/passport.js
+++ server/auth/trakt/passport.js
@@ -7,31 +7,33 @@ export function setup(User, config) {
     clientSecret: config.trakt.clientSecret,
     callbackURL: config.trakt.callbackURL
   },
-  function(accessToken, refreshToken, profile, done) {
+  function(accessToken, refreshToken, params, profile, done) {
     console.log('accessToken', accessToken);
     console.log('refreshToken', refreshToken);
+    console.log('params', params);
     console.log('profile', profile);
     console.log('done', done);
 
 
-    // User.findOne({'google.id': profile.id}).exec()
-    //   .then(user => {
-    //     if(user) {
-    //       return done(null, user);
-    //     }
-    //
-    //     user = new User({
-    //       name: profile.displayName,
-    //       email: profile.emails[0].value,
-    //       role: 'user',
-    //       username: profile.emails[0].value.split('@')[0],
-    //       provider: 'google',
-    //       google: profile._json
-    //     });
-    //     user.save()
-    //       .then(savedUser => done(null, savedUser))
-    //       .catch(err => done(err));
-    //   })
-    //   .catch(err => done(err));
+    User.findOne({'google.id': profile.id}).exec()
+      .then(user => {
+        if(user) {
+          return done(null, user);
+        }
+
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          role: 'user',
+          username: profile.emails[0].value.split('@')[0],
+          provider: 'google',
+          google: profile._json
+        });
+
+        user.save()
+          .then(savedUser => done(null, savedUser))
+          .catch(err => done(err));
+      })
+      .catch(err => done(err));
   }));
 }
